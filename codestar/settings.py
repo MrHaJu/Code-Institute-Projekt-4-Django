453a13637af0150ae91d83c38c038946fbf1fbd3
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-10-16 13:10:14 +0200

    update settings host

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['marhaju-pp4-horse-planer.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://marhaju-pp4-horse-planer-498b774ac3b9.herokuapp.com/', 'localhost']
 
 
 # Application definition
